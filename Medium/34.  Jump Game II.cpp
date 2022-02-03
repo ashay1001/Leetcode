aba9af9553d47017fb03b13d80c011736b9b95ea
Ashay <ashay165@gmail.com>
2022-02-03 23:45:59 +0530

     Jump Game II

---
+++ Medium/34.  Jump Game II.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        
+        int currReach = 0, currMax = 0, jump = 0;
+        
+        for(int i=0; i<nums.size()-1; i++)
+        {
+            if(i+nums[i] > currMax)
+                currMax = i + nums[i];
+            
+            if(i == currReach)
+            {
+                jump++;
+                currReach = currMax;
+            }
+        }
+        return jump;
+    }
+};
