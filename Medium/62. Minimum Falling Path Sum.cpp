c6fbbafb526457da0ea6dd9aeb71211bcf9d5d26
Ashay <ashay165@gmail.com>
2022-03-08 09:22:14 +0530

    Minimum Falling Path Sum

---
+++ Medium/62. Minimum Falling Path Sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+     
+        int n = matrix.size();  //n x n
+        
+        for(int i=1; i<n; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                if(j == 0)
+                    matrix[i][j] += min(matrix[i-1][j], matrix[i-1][j+1]);
+                
+                else if(j == n-1)
+                    matrix[i][j] += min(matrix[i-1][j], matrix[i-1][j-1]);
+                
+                else
+                    matrix[i][j] += min(matrix[i-1][j], min(matrix[i-1][j+1], matrix[i-1][j-1]));
+            }
+        }
+        int sum=INT_MAX;
+        for(int i=0; i<n; i++)
+            sum = min(sum, matrix[n-1][i]);
+        return sum;
+    }
+};
