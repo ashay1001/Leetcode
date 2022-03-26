291c17be68d924f7f9cf2010cc935e109fd11937
Ashay <ashay165@gmail.com>
2022-03-26 17:11:36 +0530

    Binary Search

---
+++ Easy/69. Binary Search.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+        int low = 0, high = nums.size()-1;
+        
+        if(high == 0)
+        {
+            if(nums[high] == target)
+                return 0;
+            else
+                return -1;
+        }
+        
+        while(low <= high)
+        {
+            int mid = (low + high)/2;
+            
+            if(nums[mid] == target) return mid;
+            
+            if(target > nums[mid])
+                low = mid+1;
+            else
+                high = mid-1;
+        }
+        return -1;
+    }
+};
