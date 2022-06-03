1b97959f0d1c66a1553dc46dae912d2807995435
Ashay <ashay165@gmail.com>
2022-06-03 20:51:59 +0530

    Longest Common Subsequence

--- Medium/31. Longest Common Subsequence.cpp
+++ Medium/31. Longest Common Subsequence.cpp
@@ -20,3 +20,28 @@ public:
         return dp[n][m];
     }
 };
+
+/*
+Recursive:
+
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.size(), m = text2.size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        return LCS(text1, text2, n, m, dp);
+    }
+    
+    int LCS(string &s1, string &s2, int n, int m, vector<vector<int>> &dp)
+    {
+        if(n == 0 || m == 0) return 0;
+        if(dp[n][m] != -1) return dp[n][m];
+        
+        if(s1[n-1] == s2[m-1])
+            return dp[n][m] = 1 + LCS(s1, s2, n-1, m-1, dp);
+        else
+            return dp[n][m] = max(LCS(s1,s2, n-1, m, dp), LCS(s1, s2, n, m-1, dp));
+    }
+};
+
+*/
