ba4d44579cba636fee6eca2208752201925fb658
Ashay <ashay165@gmail.com>
2022-06-04 21:21:37 +0530

    Delete Operation for Two Strings

---
+++ Medium/120. Delete Operation for Two Strings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        return word1.length() + word2.length() - 2*LCS(word1, word2);
+    }
+    
+    int LCS(string &s1, string &s2)
+	{
+	    int n = s1.size(), m = s2.size();
+	    vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+	    
+	    for(int i=1; i<n+1; i++)
+	    {
+	        for(int j=1; j<m+1; j++)
+	        {
+	            if(s1[i-1] == s2[j-1])
+	                dp[i][j] = 1 + dp[i-1][j-1];
+	            else
+	                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+	        }
+	    }
+	    return dp[n][m];
+	}
+};
