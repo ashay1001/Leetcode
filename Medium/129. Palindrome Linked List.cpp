f7c61870de23e59fbe73937dcef6d8423f5ef338
Ashay <ashay165@gmail.com>
2022-06-15 23:29:36 +0530

    Palindrome Linked List

---
+++ Medium/129. Palindrome Linked List.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        
+        if(head==NULL || head->next==NULL) return true;
+        
+        ListNode* slow = head;
+        ListNode* fast = head;
+            
+        while(fast->next!=NULL && fast->next->next!=NULL) 
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+            
+        slow->next = reverseList(slow->next);
+        slow = slow->next;
+        ListNode* dummy = head;
+            
+        while(slow != NULL) 
+        {
+            if(dummy->val != slow->val) return false;
+            dummy = dummy->next;
+            slow = slow->next;
+        }
+        return true; 
+    }
+    
+    ListNode* reverseList(ListNode *head)
+    {
+        if(!head or !head->next) return head;
+        
+        ListNode *new_head = reverseList(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        
+        return new_head;
+    }
+};
